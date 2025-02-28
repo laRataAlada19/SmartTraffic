fc6cfc3d3fbc53cdfad1be1cb25aa04d8c833cd0
SCFrancisco2221857 <2221857@my.ipleiria.pt>
2025-02-28 15:53:25 +0000

    Update teste.py
    Counter mas ainda sem tracking por isso valores errados apresentados no fim

--- teste.py
+++ teste.py
@@ -1,5 +1,19 @@
 from ultralytics import YOLO
 
 model = YOLO('yolov8n.pt')  
-model.predict('TestVideo.mp4', show=True)
+results=model.predict('TestVideo.mp4', show=True)
 #pequeno teste para verificar se o modelo está funcionar corretamente
+
+#codigo para guardar classes
+class_counter={}
+
+for result in results:
+    for class_id in result.boxes.cls:
+        class_name= model.names[int(class_id)]
+        if class_name in class_counter:
+            class_counter[class_name] += 1
+        else:
+            class_counter[class_name] = 1
+
+for class_name, count in class_counter.items():
+    print(f"Classe: {class_name}, Contagem: {count}")
