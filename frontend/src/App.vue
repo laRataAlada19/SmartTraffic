53b59b04bcb0b3e0667c64b41e1b56561dbb5c42
SCFrancisco2221857 <2221857@my.ipleiria.pt>
2025-04-02 20:48:03 +0100

    init do vue e laravel

---
+++ frontend/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <p>SmartTraffic</p>
+</template>
+
